78152b8e161d643176c7c343c5a8880a508ffc08
Andrew Djoga <andrew.djoga@gmail.com>
2013-01-02 18:54:58 +0200

    Datatables server-side processing

--- app.rb
+++ app.rb
@@ -1,5 +1,23 @@
 require 'sinatra'
+require "sinatra/json"
+require 'sinatra/reloader' if development?
 
 get '/' do
   erb :index
 end
+
+get '/list.json' do
+  json({
+    aaData: LANGUAGES,
+    sEcho: params[:sEcho],
+    iTotalRecords: LANGUAGES.size,
+    iTotalDisplayRecords: LANGUAGES.size
+  })
+end
+
+LANGUAGES = [
+  ['Erlang', 'strong', 'dynamic'],
+  ['Java', 'strong', 'static'],
+  ['JavaScript', 'weak', 'dynamic'],
+  ['Ruby', 'strong', 'dynamic']
+]
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,8 +1,12 @@
 $(function () {
   $('table.languages').dataTable({
     bFilter: false,
+    bSort: false,
     bInfo: false,
     bLengthChange: false,
-    iDisplayLength: 100
+    iDisplayLength: 100,
+    bProcessing: true,
+    bServerSide: true,
+    sAjaxSource: '/list.json'
   })
 })
