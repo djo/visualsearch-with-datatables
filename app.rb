c3fccfd45ca6417cfef699a6549e9d3d90f5302f
Andrew Djoga <andrew.djoga@gmail.com>
2013-01-02 20:44:50 +0200

    VisualSearch stuff

--- app.rb
+++ app.rb
@@ -7,17 +7,43 @@ get '/' do
 end
 
 get '/list.json' do
+  data = Languages.new(params[:search]).filter
+
   json({
-    aaData: LANGUAGES,
+    aaData: data,
     sEcho: params[:sEcho],
-    iTotalRecords: LANGUAGES.size,
-    iTotalDisplayRecords: LANGUAGES.size
+    iTotalRecords: Languages::DATA.length,
+    iTotalDisplayRecords: data.size
   })
 end
 
-LANGUAGES = [
-  ['Erlang', 'strong', 'dynamic'],
-  ['Java', 'strong', 'static'],
-  ['JavaScript', 'weak', 'dynamic'],
-  ['Ruby', 'strong', 'dynamic']
-]
+class Languages
+  DATA = [
+    ['Erlang', 'strong', 'dynamic'],
+    ['Java', 'strong', 'static'],
+    ['JavaScript', 'weak', 'dynamic'],
+    ['Ruby', 'strong', 'dynamic']
+  ]
+
+  COLUMNS = { 'strength' => 1, 'checking' => 2 }
+
+  def initialize(conditions)
+    @conditions = conditions || {}
+  end
+
+  def filter
+    return DATA if @conditions.length == 0
+    DATA.select { |language| match?(language) }
+  end
+
+  private
+
+  def match?(language)
+    @conditions.each do |name, options|
+      value = language[COLUMNS[name]]
+      return false unless options.include?(value)
+    end
+
+    true
+  end
+end
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,5 +1,26 @@
 $(function () {
-  $('table.languages').dataTable({
+  var table, visualSearch
+
+  visualSearch = VS.init({
+    container : $('.visualsearch'),
+    query     : '',
+    callbacks : {
+      search: function() {
+        table.fnDraw()
+      },
+      facetMatches: function(callback) {
+        callback(['strength', 'checking'])
+      },
+      valueMatches: function(facet, searchTerm, callback) {
+        switch (facet) {
+          case 'strength': callback(['strong', 'weak']);    break
+          case 'checking': callback(['static', 'dynamic']); break
+        }
+      }
+    }
+  })
+
+  table = $('table.languages').dataTable({
     bFilter: false,
     bSort: false,
     bInfo: false,
@@ -7,6 +28,19 @@ $(function () {
     iDisplayLength: 100,
     bProcessing: true,
     bServerSide: true,
-    sAjaxSource: '/list.json'
+    sAjaxSource: '/list.json',
+    fnServerParams: function (aoData) {
+      var facets = visualSearch.searchQuery.facets()
+
+      $.each(facets, function (index, facet) {
+        var name  = _.keys(facet)[0],
+            value = _.values(facet)[0]
+
+        aoData.push({
+          name: 'search[' + name + '][]',
+          value: value
+        })
+      })
+    }
   })
 })
